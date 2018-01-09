36f5f777dc692aa1af3584bf48db53212798752b
maevadevs <maevadevs@users.noreply.github.com>
2018-01-09 15:44:03 -0600

    add functions

---
+++ functions.js
@@ -0,0 +1,7 @@
+// Overview
+// ********
+
+// Prior to ECMAScript 6, JavaScript functions hadn’t changed much since the language was created
+// ES6 functions make a big leap forward
+// A number of incremental improvements on top of ES5 functions
+// Make programming in JavaScript less error-prone and more powerful
